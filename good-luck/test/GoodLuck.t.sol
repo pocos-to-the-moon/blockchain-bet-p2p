3cee9544ffcca7c916e72d8a8d3a2562571c6a2a
jeanmamelo <jeanmamelo@gmail.com>
2024-07-11 05:44:23 -0300

    chore: rename smart contract

---
+++ good-luck/test/GoodLuck.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {GoodLuck} from "../src/GoodLuck.sol";
+
+contract GoodLuckTest is Test {
+}
